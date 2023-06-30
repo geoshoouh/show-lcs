52435a2a17cf238ecc58f6d7f34274ee8e4ae4b0
geoshoouh <joshua.d.tate12@gmail.com>
2023-06-29 20:15:55 -0500

    RandomSequenceInput formatting

--- src/components/RandomSequenceInput.tsx
+++ src/components/RandomSequenceInput.tsx
@@ -15,18 +15,18 @@ export function RandomSequenceInput(props) {
 
     return (
         <>
-            <div className="input-group mb-3">
-              <span className="input-group-text">Length 1</span>
-              <input id="random-length-1" type="number" className="form-control" placeholder="Length of first sequence" onChange={ () => computeLcs() }/>
-              <span className="input-group-text">Variance 1</span>
-              <input id="random-variance-1" type="number" className="form-control" placeholder="Variance level of first sequence" onChange={ () => computeLcs() }/>
-            </div>
-            <div className="input-group mb-3">
-              <span className="input-group-text">Length 2</span>
-              <input id="random-length-2" type="number" className="form-control" placeholder="Length of second sequence" onChange={ () => computeLcs() }/>
-              <span className="input-group-text">Variance 2</span>
-              <input id="random-variance-2" type="number" className="form-control" placeholder="Variance level of second sequence" onChange={ () => computeLcs() }/>
-            </div>
-          </>
+          <div className="input-group mb-3">
+            <span className="input-group-text">Length 1</span>
+            <input id="random-length-1" type="number" className="form-control" placeholder="Length of first sequence" onChange={ () => computeLcs() }/>
+            <span className="input-group-text">Variance 1</span>
+            <input id="random-variance-1" type="number" className="form-control" placeholder="Variance level of first sequence" onChange={ () => computeLcs() }/>
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text">Length 2</span>
+            <input id="random-length-2" type="number" className="form-control" placeholder="Length of second sequence" onChange={ () => computeLcs() }/>
+            <span className="input-group-text">Variance 2</span>
+            <input id="random-variance-2" type="number" className="form-control" placeholder="Variance level of second sequence" onChange={ () => computeLcs() }/>
+          </div>
+        </>
     );
 }
